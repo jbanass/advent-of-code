f4f342ca50b0f99d50f2983150bf858a9838e9cc
Joseph Banass <jbanass@benchprep.com>
2020-12-06 21:13:33 -0600

    Refactor input to separate file

--- 2020/01/script_spec.rb
+++ 2020/01/script_spec.rb
@@ -1,27 +1,30 @@
 require_relative 'script.rb'
 
 describe DayOne do
+  let(:input) {
+    [
+      1721,
+      979,
+      366,
+      299,
+      675,
+      1456
+    ]
+  }
+
   describe "#calculate_two" do
     it "should calculate correctly" do
-      expect(described_class.calculate_two([1721,
-        979,
-        366,
-        299,
-        675,
-        1456])).to eq(514579)
+      allow(described_class).to receive(:numbers).and_return(input)
+
+      expect(described_class.calculate_two).to eq(514579)
     end
   end
   
   describe "#calculate_three" do
     it "should calculate correctly" do
-      expect(described_class.calculate_three([1721,
-        979,
-        366,
-        299,
-        675,
-        1456])).to eq(241861950)
+      allow(described_class).to receive(:numbers).and_return(input)
+
+      expect(described_class.calculate_three).to eq(241861950)
     end
   end
-
-
 end
