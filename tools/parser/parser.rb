be69ed582ce8a1af624cbdc63f6d4306daac4b11
Joseph Banass <jbanass@benchprep.com>
2020-12-06 20:28:58 -0600

    Add parser and specs

---
+++ tools/parser/parser.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+class Parser
+
+  def self.split_on_newline(file_path)
+    strip_lines(read_file(file_path).split("\n"))
+  end
+
+  def self.split_on_consecutive_newlines(file_path)
+    read_file(file_path).split("\n\n").map { |line| strip_lines(line.split("\n")) }
+  end
+
+  def self.read_file(file_path)
+    file_path = File.join(File.expand_path(File.dirname(__FILE__)), file_path)
+    File.open(file_path).read
+  end
+
+  def self.strip_lines(lines)
+    lines.map { |line| line.strip }
+  end
+end
