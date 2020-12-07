5c65f62fd9f16022cd119a7f15a60f1d7c84b079
Joseph Banass <jbanass@benchprep.com>
2020-12-06 22:10:08 -0600

    Refactor to use parser, input file

--- 2020/03/script_spec.rb
+++ 2020/03/script_spec.rb
@@ -1,4 +1,5 @@
 require_relative "script.rb"
+require_relative "../../tools/parser/parser.rb"
 require 'pry'
 
 describe DayThree do
@@ -23,7 +24,8 @@ describe DayThree do
       let(:expected_answer) { 7 }
 
       it "correctly calculates" do
-        expect(described_class.calculate(input, 3, 1)).to eq(expected_answer)
+        allow(Parser).to receive(:split_on_newline).and_return(input)
+        expect(described_class.calculate(3, 1)).to eq(expected_answer)
       end
     end
     
@@ -31,28 +33,32 @@ describe DayThree do
     context "when slope is right 1 down 1" do
       let(:expected_answer) { 2 }
       it "correctly calculates" do
-        expect(described_class.calculate(input, 1, 1)).to eq(expected_answer)
+        allow(Parser).to receive(:split_on_newline).and_return(input)
+        expect(described_class.calculate(1, 1)).to eq(expected_answer)
       end
     end
 
     context "when slope is right 5 down 1" do
       let(:expected_answer) { 3 }
       it "correctly calculates" do
-        expect(described_class.calculate(input, 5, 1)).to eq(expected_answer)
+        allow(Parser).to receive(:split_on_newline).and_return(input)
+        expect(described_class.calculate(5, 1)).to eq(expected_answer)
       end
     end
 
     context "when slope is right 7 down 1" do
       let(:expected_answer) { 4 }
       it "correctly calculates" do
-        expect(described_class.calculate(input, 7, 1)).to eq(expected_answer)
+        allow(Parser).to receive(:split_on_newline).and_return(input)
+        expect(described_class.calculate(7, 1)).to eq(expected_answer)
       end
     end
 
     context "when slope is right 1 down 2" do
       let(:expected_answer) { 2 }
       it "correctly calculates" do
-        expect(described_class.calculate(input, 1, 2)).to eq(expected_answer)
+        allow(Parser).to receive(:split_on_newline).and_return(input)
+        expect(described_class.calculate(1, 2)).to eq(expected_answer)
       end
     end
   end
