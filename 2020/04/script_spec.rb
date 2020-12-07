35b834f0d0a17fa216f978f87e70afdbf1f058bf
Joseph Banass <jbanass@benchprep.com>
2020-12-06 22:22:22 -0600

    Refactor to use parser, input file

--- 2020/04/script_spec.rb
+++ 2020/04/script_spec.rb
@@ -1,42 +1,44 @@
 require_relative 'script.rb'
+require_relative '../../tools/parser/parser.rb'
 
 describe DayFour do
   let(:input) {
-    <<~STRING
-      eyr:1972 cid:100
-      hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+    [
+      %w(eyr:1972 cid:100
+      hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926),
       
-      iyr:2019
+      %w(iyr:2019
       hcl:#602927 eyr:1967 hgt:170cm
-      ecl:grn pid:012533040 byr:1946
+      ecl:grn pid:012533040 byr:1946),
       
-      hcl:dab227 iyr:2012
-      ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+      %w(hcl:dab227 iyr:2012
+      ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277),
       
-      hgt:59cm ecl:zzz
+      %w(hgt:59cm ecl:zzz
       eyr:2038 hcl:74454a iyr:2023
-      pid:3556412378 byr:2007
+      pid:3556412378 byr:2007),
 
-      pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
-      hcl:#623a2f
+      %w(pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+      hcl:#623a2f),
 
-      eyr:2029 ecl:blu cid:129 byr:1989
-      iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+      %w(eyr:2029 ecl:blu cid:129 byr:1989
+      iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm),
 
-      hcl:#888785
+      %w(hcl:#888785
       hgt:164cm byr:2001 iyr:2015 cid:88
       pid:545766238 ecl:hzl
-      eyr:2022
+      eyr:2022),
 
-      iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
-  STRING
+      %w(iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719)
+    ]
   }
 
   describe "#calculate" do
     let(:expected_result) { 4 }
 
     it "calculates correctly" do
-      expect(described_class.calculate(input)).to eq(expected_result)
+      allow(Parser).to receive(:split_on_consecutive_newlines).and_return(input)
+      expect(described_class.calculate).to eq(expected_result)
     end
   end
 
